7334f62fdbcae84f065d4ef8b0f92e113595d808
trnciii <doityan223@gmail.com>
2023-01-26 05:56:08 +0900

    declip escape seqs before measureing display length

--- zen.py
+++ zen.py
@@ -1,7 +1,8 @@
 import unicodedata as uc
+from .terminal import declip
 
 def display_length(s):
-	return sum( map(lambda x:int(x in 'FW')+1, map(uc.east_asian_width, s)) )
+	return sum( map(lambda x:int(x in 'FW')+1, map(uc.east_asian_width, declip(s))) )
 
 def ljust(s, w):
 	return s + ' '*max(0, w-display_length(s))
