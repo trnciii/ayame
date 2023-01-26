dd8f6e923732f38fb4deb8ffff50ef44923ad397
trnciii <doityan223@gmail.com>
2023-01-27 04:59:42 +0900

    add trim funtion

--- zen.py
+++ zen.py
@@ -1,11 +1,15 @@
 import unicodedata as uc
-from .terminal import declip
+from .terminal import declip, ptn_escape
 
-def to_width(r):
+def ret_to_width(r):
 	return 1 + int(r in 'FW')
 
+def char_to_width(c):
+	return 1 + int(uc.east_asian_width(c) in 'FW')
+
+
 def display_length(s):
-	return sum(map(to_width, map(uc.east_asian_width, declip(s))))
+	return sum(map(ret_to_width, map(uc.east_asian_width, declip(s))))
 
 
 def ljust(s, w):
@@ -13,3 +17,20 @@ def ljust(s, w):
 
 def rjust(s, w):
 	return ' '*max(0, w-display_length(s)) + s
+
+
+def trim(s, w):
+	default = (len(s), 0)
+	spans = (m.span() for m in ptn_escape.finditer(s))
+
+	i = 0
+	l = 0
+	b, e = next(spans, default)
+	while i < len(s) and l < w:
+		if b <= i:
+			i = e
+			b, e = next(spans, default)
+		else:
+			l += char_to_width(s[i])
+			i += 1
+	return s[:i-1 if l>w else i]
