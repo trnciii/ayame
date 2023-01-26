3e739fde49f400c44ec810995ba29289bb87241b
trnciii <doityan223@gmail.com>
2023-01-27 04:17:44 +0900

    create result to width function

--- zen.py
+++ zen.py
@@ -1,8 +1,12 @@
 import unicodedata as uc
 from .terminal import declip
 
+def to_width(r):
+	return 1 + int(r in 'FW')
+
 def display_length(s):
-	return sum( map(lambda x:int(x in 'FW')+1, map(uc.east_asian_width, declip(s))) )
+	return sum(map(to_width, map(uc.east_asian_width, declip(s))))
+
 
 def ljust(s, w):
 	return s + ' '*max(0, w-display_length(s))
